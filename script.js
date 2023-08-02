7245127ea0eb4e811f5347cf4da145e9d565b449
Daniel Clapp <danielclapp94@gmail.com>
2023-08-02 01:29:32 -0700

    final version

--- script.js
+++ script.js
@@ -104,19 +104,23 @@ const VoiceRSS = {
   },
 };
 
-// function test() {
-//   VoiceRSS.speech({
-//     key: '6297b78b27b140e0b3dd8d1b24b6355b',
-//     src: 'Hello, world!',
-//     hl: 'en-us',
-//     v: 'Linda',
-//     r: 0,
-//     c: 'mp3',
-//     f: '44khz_16bit_stereo',
-//     ssml: false,
-//   });
-// }
-// test();
+function toggleButton() {
+  button.disabled = !button.disabled;
+}
+
+function tellMe(joke) {
+  console.log('tell me:', joke);
+  VoiceRSS.speech({
+    key: '6297b78b27b140e0b3dd8d1b24b6355b',
+    src: joke,
+    hl: 'en-us',
+    v: 'Linda',
+    r: 0,
+    c: 'mp3',
+    f: '44khz_16bit_stereo',
+    ssml: false,
+  });
+}
 
 async function getJokes() {
   let joke = '';
@@ -129,9 +133,12 @@ async function getJokes() {
     } else {
       joke = data.joke;
     }
-    console.log(joke);
+    tellMe(joke);
+    toggleButton();
   } catch (error) {
     console.log('error');
   }
 }
-getJokes();
+
+button.addEventListener('click', getJokes);
+audioElement.addEventListener('ended', toggleButton);
