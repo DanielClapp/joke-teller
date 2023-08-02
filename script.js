0327448f18be8af216576b7c0fc8657007e68580
Daniel Clapp <danielclapp94@gmail.com>
2023-08-02 02:38:12 -0700

    removed .gitignore file

--- script.js
+++ script.js
@@ -5,6 +5,19 @@ function toggleButton() {
   button.disabled = !button.disabled;
 }
 
+function tellMe(joke) {
+  VoiceRSS.speech({
+    key: '6297b78b27b140e0b3dd8d1b24b6355b',
+    src: joke,
+    hl: 'en-us',
+    v: 'Linda',
+    r: 0,
+    c: 'mp3',
+    f: '44khz_16bit_stereo',
+    ssml: false,
+  });
+}
+
 async function getJokes() {
   let joke = '';
   const apiUrl = 'https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark';
